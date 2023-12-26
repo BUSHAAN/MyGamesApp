72aaf0e88e78652fc839ad5e9635724d4ac10021
Bushaan Gunatilake <bushaangunatilake@gmail.com>
2023-12-26 22:09:18 +0530

    add clickable buttons on genre list

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,41 +1,56 @@
 import {
+    Box,
     Button,
     HStack,
     Image,
     List,
     ListItem,
     Spinner,
+    Text,
 } from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
-const GenreList = ({onSelectGenre}:Props) => {
+const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
   if (error) return null;
   if (isLoading) return <Spinner />;
   return (
     <List>
       {data.map((genre) => (
-        <ListItem key={genre.id}>
-          <HStack paddingY="5px">
-            <Image
-              boxSize="32px"
-              borderRadius={8}
-              src={getCroppedImageUrl(genre.image_background)}
-            />
-            <Button
-              fontSize="lg"
-              variant="link"
-              onClick={() => onSelectGenre(genre)}
-            >
-              {genre.name}
-            </Button>
-          </HStack>
-        </ListItem>
+        <Button  variant="link" onClick={() => onSelectGenre(genre)}>
+          <Box
+            bg={genre.id === selectedGenre?.id ? "#2D3748" : "transparent"}
+            w="180px"
+            borderRadius={3}
+          >
+            <ListItem key={genre.id}>
+              <HStack padding="5px">
+                <Image
+                  boxSize="32px"
+                  borderRadius={8}
+                  src={getCroppedImageUrl(genre.image_background)}
+                />
+
+                <Text
+                  fontSize="lg"
+                  fontWeight={
+                    genre.id === selectedGenre?.id ? "bold" : "normal"
+                  }
+                >
+                  {genre.slug === "massively-multiplayer"
+                    ? "Multiplayer"
+                    : genre.name}
+                </Text>
+              </HStack>
+            </ListItem>
+          </Box>
+        </Button>
       ))}
     </List>
   );
