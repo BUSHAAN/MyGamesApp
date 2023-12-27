dfc8ccdb03486f308c3a3e22abe14318ccac4f47
Bushaan Gunatilake <bushaangunatilake@gmail.com>
2023-12-27 21:02:26 +0530

    Add a dynamic game heading

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,10 +10,10 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card>
+    <Card height={'100%'}>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
+        <Heading fontSize="2xl" >{game.name}</Heading>
         <HStack justifyContent="space-between">
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
