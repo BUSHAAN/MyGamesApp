2955107716ed8acb55cb6fe8a50e63309db9aa7c
Bushaan Gunatilake <bushaangunatilake@gmail.com>
2023-12-27 21:29:00 +0530

    Cleanup game cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,19 +10,20 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card height={'100%'}>
+    <Card height='100%'>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-        <Heading fontSize="2xl" >{game.name}</Heading>
-        <HStack justifyContent="space-between">
+        <HStack justifyContent="space-between" marginBottom={3}>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
           <CriticScore score={game.metacritic} />
         </HStack>
-      </CardBody>
-    </Card>
-  );
+        <Heading fontSize="2xl" >{game.name}</Heading>
+        </CardBody>
+  </Card>
+);
+
 };
 
 export default GameCard;
