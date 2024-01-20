bd944b81ea16342622054539769e481c43dc4702
Bushaan Gunatilake <bushaangunatilake@gmail.com>
2024-01-20 16:26:33 +0530

    minor errors fixed

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 export default axios.create({
+    
     baseURL:'https://api.rawg.io/api',
     params:{
-        key:'ffb35ede00e646be815b12b6af6bb9f7'
+        key:import.meta.env.VITE_API_KEY
     }
 })
